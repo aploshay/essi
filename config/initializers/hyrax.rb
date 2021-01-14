fc30bb6ed14be455486d5380b361f1b7e3a6fbf0
Adam Ploshay <aploshay@iu.edu>
2021-01-14 12:57:17 -0500

    WIP

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -197,7 +197,7 @@ Hyrax.config do |config|
   # config.audit_user_key = 'audituser@example.com'
   #
   # The banner image. Should be 5000px wide by 1000px tall
-  # config.banner_image = 'https://cloud.githubusercontent.com/assets/92044/18370978/88ecac20-75f6-11e6-8399-6536640ef695.jpg'
+  config.banner_image = 'https://cdn.pixabay.com/photo/2015/11/02/18/34/banner-1018818_640.jpg' # FIXME
 
   # Temporary paths to hold uploads before they are ingested into FCrepo
   # These must be lambdas that return a Pathname. Can be configured separately
