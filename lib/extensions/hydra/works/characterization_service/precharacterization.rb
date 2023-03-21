5c90b3a34882864d5588e6bb82ed8a8817239c94
Adam Ploshay <aploshay@iu.edu>
2023-03-21 17:12:24 -0400

    proof of concept: patch to get file characterization from existing file

--- lib/extensions/hydra/works/characterization_service/precharacterization.rb
+++ lib/extensions/hydra/works/characterization_service/precharacterization.rb
@@ -1,4 +1,4 @@
-# unmodified from Hydra::Works
+# modified from Hydra::Works
 module Extensions
   module Hydra::Works
     module CharacterizationService
@@ -9,10 +9,28 @@ module Extensions
         # Get the terms (and their values) from the extracted metadata
         # Assign the values of the terms to the properties of the object
         def characterize
-          content = source_to_content
-          extracted_md = extract_metadata(content)
+          existing_file = precharacterization(source)
+          if existing_file
+            extracted_md = File.read(existing_file)
+          else
+            content = source_to_content
+            extracted_md = extract_metadata(content)
+          end
           terms = parse_metadata(extracted_md)
           store_metadata(terms)
+        end
+  
+        def precharacterization(filename)
+          Rails.logger.info 'Checking for a Pre-derived characterization folder.'
+          return false unless ESSI.config.dig(:essi, :characterization_folder)
+    
+          Rails.logger.info 'Checking for a Pre-derived characterization file.'
+          characterization_filename = "#{File.basename(filename, '.*')}-characterization.xml"
+          characterization_file = File.join(ESSI.config.dig(:essi, :characterization_folder), characterization_filename)
+          return false unless File.exist?(characterization_file)
+    
+          Rails.logger.info 'Using Pre-derived characterization file.'
+          characterization_file
         end
       end
     end
