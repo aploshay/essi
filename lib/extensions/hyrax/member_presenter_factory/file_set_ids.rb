551b093faf554616421355a681a38c5b3adf41bd
Adam Ploshay <aploshay@iu.edu>
2023-05-12 14:12:26 -0400

    WIP possibly unnecessary: fall back to query if index missing

--- lib/extensions/hyrax/member_presenter_factory/file_set_ids.rb
+++ lib/extensions/hyrax/member_presenter_factory/file_set_ids.rb
@@ -1,3 +1,4 @@
+# modified from stock hyrax to use index, if available
 module Extensions
   module Hyrax
     module MemberPresenterFactory
@@ -6,7 +7,15 @@ module Extensions
         # in order.
         # Arbitrarily maxed at 10 thousand; had to specify rows due to solr's default of 10
         def file_set_ids
-          @file_set_ids ||= @work['file_set_ids_ssim']
+          @file_set_ids ||= @work['file_set_ids_ssim'] || query_file_set_ids
+        end
+
+        def query_file_set_ids
+         ActiveFedora::SolrService.query("{!field f=has_model_ssim}FileSet",
+                                         rows: 10_000,
+                                         fl: ActiveFedora.id_field,
+                                         fq: "{!join from=ordered_targets_ssim to=id}id:\"#{id}/list_source\"")
+                                  .flat_map { |x| x.fetch(ActiveFedora.id_field, []) }
         end
       end
     end
