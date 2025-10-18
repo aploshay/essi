091460a78567bb7c89363a0e791f393580f8a178
Adam Ploshay <aploshay@iu.edu>
2025-10-18 00:31:21 -0400

    [ESSI-2015] specs for xml parser

--- spec/parsers/bulkrax/mets_xml_parser_spec.rb
+++ spec/parsers/bulkrax/mets_xml_parser_spec.rb
@@ -4,46 +4,31 @@ require 'rails_helper'
 
 module Bulkrax
   RSpec.describe MetsXmlParser do
-    describe '#create_works' do
-      subject(:xml_parser) { described_class.new(importer) }
-      let(:importer) { create(:bulkrax_importer_mets_xml) }
-      let(:entry) { create(:bulkrax_entry, importerexporter: importer) }
+    subject(:xml_parser) { described_class.new(importer) }
 
-      before do
-        Bulkrax.default_work_type = 'Work'
-        Bulkrax.field_mappings['Bulkrax::MetsXmlParser'] = {
-          "source" => { from: ["OBJID"], source_identifier: true },
-          "source_identifier" => { from: ["identifier"] },
-          "work_type" => 'PagedResource'
-        }
-
-        allow(Bulkrax::MetsXmlEntry).to receive_message_chain(:where, :first_or_create!).and_return(entry)
-        allow(entry).to receive(:id)
-        allow(Bulkrax::ImportWorkJob).to receive(:perform_later)
+    describe '#entry_class' do
+      it 'returns Bulkrax::MetsXmlEntry' do
+        expect(subject.entry_class).to eq Bulkrax::MetsXmlEntry
       end
+    end
 
-      context 'with good data' do
-        before do
-          importer.parser_fields = {
-            'import_file_path' => './spec/fixtures/xml/mets.xml',
-            'record_element' => 'mets:mets'
-          }
+    describe '#create_relationships' do
+      before do
+        allow(subject).to receive(:parser_fields).and_return(parser_fields.with_indifferent_access)
+        allow(ScheduleRelationshipsJob).to receive(:set).and_return(double('PerformLater', perform_later: true))
+      end
+      context 'with no collection_id' do
+        let(:parser_fields) { {} }
+        it 'does not call ScheduleRelationshipsJob' do
+          expect(ScheduleRelationshipsJob).not_to receive(:set)
+          subject.create_relationships
         end
-
-        context 'and import_type set to single' do
-          before do
-            importer.parser_fields.merge!('import_type' => 'single')
-          end
-
-          it 'processes the line' do
-            expect(xml_parser).to receive(:increment_counters).once
-            xml_parser.create_works
-          end
-
-          it 'counts the correct number of works and collections' do
-            expect(xml_parser.total).to eq(1)
-            expect(xml_parser.collections_total).to eq(0)
-          end
+      end
+      context 'with a collection_id' do
+        let(:parser_fields) { { collection_id: 'cid' } }
+        it 'calls ScheduleRelationshipJob' do
+          expect(ScheduleRelationshipsJob).to receive(:set)
+          subject.create_relationships
         end
       end
     end
