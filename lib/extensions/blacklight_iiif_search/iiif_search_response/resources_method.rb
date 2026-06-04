495bb76824a2d7d35c4a8364cc25a108ec48dd67
Adam Ploshay <aploshay@iu.edu>
2026-06-04 17:41:53 -0400

    [ESSI-2209] [ESSI-2260] apply patch for iiif search results

--- lib/extensions/blacklight_iiif_search/iiif_search_response/resources_method.rb
+++ lib/extensions/blacklight_iiif_search/iiif_search_response/resources_method.rb
@@ -1,4 +1,5 @@
-# unmodified from blacklight_iiif_search v1.0.0
+# modified from blacklight_iiif_search v1.0.0
+# changes behavior to return every match on a page, instead of only the first
 module Extensions
   module BlacklightIiifSearch
     module IiifSearchResponse
@@ -11,6 +12,7 @@ module Extensions
           solr_response['highlighting'].each do |id, hl_hash|
             hit = { '@type': 'search:Hit', 'annotations': [] }
             document = solr_response.documents.select { |v| v[:id] == id }.first
+            hl_hash = substitute_hash(hl_hash, document)
             if hl_hash.empty?
               @total += 1
               annotation = ::BlacklightIiifSearch::IiifSearchAnnotation.new(document,
@@ -35,6 +37,24 @@ module Extensions
             @hits << hit
           end
           @resources
+        end
+
+        def substitute_hash(hash, document)
+          annotation = ::BlacklightIiifSearch::IiifSearchAnnotation.new(document,
+                                                                        solr_response.params['q'],
+                                                                        0, nil, controller,
+                                                                        @parent_document)
+
+          coords_json = annotation.send :fetch_and_parse_coords
+          query = annotation.query.gsub(annotation.additional_query_terms, '')
+          query_terms = query.split(' ').map(&:downcase)
+          matches = coords_json['coords'].select { |k, _v| k.downcase =~ /(#{query_terms.join('|')})/ }
+          if matches.size > 1
+            coords_array = matches.values.flatten(1)
+            { _key: Array.new(coords_array.size, nil) }
+          else
+            hash
+          end
         end
       end
     end
